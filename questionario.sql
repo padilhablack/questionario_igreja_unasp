82ddc216ce1729801d2a2d79f4f8de4804fcf15a
Jonas Padilha <padilhamidia@hotmail.com>
2014-07-15 23:41:59 -0300

    primeiro commit

--- questionario.sql
+++ questionario.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 
--- Versão do Servidor: 5.5.24-log
--- Versão do PHP: 5.3.13
+-- Tempo de Geração: 16/07/2014 às 02:40
+-- Versão do servidor: 5.6.12-log
+-- Versão do PHP: 5.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,39 +17,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Banco de Dados: `questionario`
+-- Banco de dados: `questionario`
 --
+CREATE DATABASE IF NOT EXISTS `questionario` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `questionario`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `questao3`
+-- Estrutura para tabela `questao3`
 --
 
 CREATE TABLE IF NOT EXISTS `questao3` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `adjetivo` varchar(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
--- Extraindo dados da tabela `questao3`
+-- Fazendo dump de dados para tabela `questao3`
 --
 
 INSERT INTO `questao3` (`id`, `adjetivo`) VALUES
-(1, '3'),
-(2, '11'),
-(3, '12'),
-(4, '5'),
-(5, '6'),
-(6, '7'),
-(7, '8'),
-(8, '3');
+(1, '9'),
+(2, '10'),
+(3, '3'),
+(4, '11'),
+(5, '4'),
+(6, '12'),
+(7, '5'),
+(8, '6'),
+(9, '4');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `respostas`
+-- Estrutura para tabela `respostas`
 --
 
 CREATE TABLE IF NOT EXISTS `respostas` (
@@ -64,12 +67,12 @@ CREATE TABLE IF NOT EXISTS `respostas` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Extraindo dados da tabela `respostas`
+-- Fazendo dump de dados para tabela `respostas`
 --
 
 INSERT INTO `respostas` (`id`, `1`, `2`, `3`, `4`, `5`, `6`) VALUES
-(1, '1', '1', '2', '1', '2', '1'),
-(2, '1', '2', '2', '2', '3', '2');
+(1, '1', '1', '2', '1', '2', '2'),
+(2, '1', '1', '2', '1', '2', '2');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
